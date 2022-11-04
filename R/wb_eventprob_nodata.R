1b715dcd4cf96eae87a7dc2bbd7f2a6df6582ed8
Jan Brederecke <58463668+janbrederecke@users.noreply.github.com>
2022-11-04 13:02:49 +0100

    Some reformatting, next documentation

--- R/wb_eventprob_nodata.R
+++ R/wb_eventprob_nodata.R
@@ -2,66 +2,67 @@
 #'
 #' @description Retrieves the event probabilites without the original data
 #'
-#' @param wb_info The output of a former wb_get_info() function.
+#' @param .wb_info The output of a former wb_get_info() function.
 #' package.
-#' @param timepoint The desired timepoint for the event probabilities.
-#' @param newdata 
-#' @param timestart
+#' @param .timepoint The desired timepoint for the event probabilities.
+#' @param .newdata
+#' @param .newdata_timestart
 #' @export
 #'
-wb_eventprob_nodata <-
-  function(wb_info,
-           timepoint = 10,
-           newdata,
-           newdata_timestart) {
+
+wb_eventprob_nodata <- function(.wb_info
+                              , .timepoint = 10
+                              , .newdata
+                              , .newdata_timestart
+    ){
     # Add error message in case type is != counting
     mssg <- "The model uses wrong type of censoring"
-    if (attr(wb_info[["fit"]]$y, "type") != "counting")
+    if (attr(.wb_info[["fit"]]$y, "type") != "counting")
       stop(mssg)
-    
+
     # Make necessary objects
-    n_newdata <- nrow(newdata)
+    n_newdata <- nrow(.newdata)
     linear_predictor_exp <-
       a_newdata <-
       b_newdata <- lp_vector <- vector(length = n_newdata)
     rm(n_newdata)
-    
+
     # Calculate linear predictor in newdata
-    for (i in 1:nrow(newdata)) {
-      stratum <- newdata[i, wb_info[["strata_variable"]]]
-      
+    for (i in seq_len(.newdata)) {
+      stratum <- .newdata[i, .wb_info[["strata_variable"]]]
+
       ## Add the Wb parameters by stratum and row in newdata
       a_newdata[i] <-
-        wb_info[["wb_param"]][paste0(wb_info[["strata_variable"]],
+        .wb_info[["wb_param"]][paste0(.wb_info[["strata_variable"]],
                                      "=",
                                      stratum), ][[1]]
       b_newdata[i] <-
-        wb_info[["wb_param"]][paste0(wb_info[["strata_variable"]],
+        .wb_info[["wb_param"]][paste0(.wb_info[["strata_variable"]],
                                      "=",
                                      stratum), ][[2]]
-      
+
       ## Calculate linear predictor per person using strata-means
-      for (j in seq_along(wb_info[["model_coeff"]])) {
-        coefficient <- wb_info[["model_coeff"]][j]
+      for (j in seq_along(.wb_info[["model_coeff"]])) {
+        coefficient <- .wb_info[["model_coeff"]][j]
         mean <-
-          unlist(wb_info[["means"]][[paste0(wb_info[["strata_variable"]],
+          unlist(.wb_info[["means"]][[paste0(.wb_info[["strata_variable"]],
                                             "=",
                                             stratum)]][j])
         value <-
-          newdata[i, colnames(wb_info[["model_coeff"]])[j]]
-        
+          .newdata[i, colnames(.wb_info[["model_coeff"]])[j]]
+
         lp_part <- (value - mean) * coefficient
-        
+
         lp_vector[j] <- lp_part
       }
       linear_predictor_exp[i] <- exp(sum(lp_vector))
     }
     rm(i, j, stratum, coefficient, mean, value, lp_part, lp_vector)
-    
+
     # Predict the event-probability
-    timestart <- newdata[, newdata_timestart]
-    1 - exp(-(((
-      timestart + timepoint
+    timestart <- .newdata[, .newdata_timestart]
+    1 - exp(- (((
+      timestart + .timepoint
     ) / b_newdata) ^ a_newdata -
       (timestart / b_newdata) ^ a_newdata) * linear_predictor_exp)
   }
--- R/wb_get_info.R
+++ R/wb_get_info.R
@@ -2,140 +2,139 @@
 #'
 #' @description Generic function to create HTML tables from lists of results
 #'
-#' @param fit
-#' @param timestart
-#' @param timestop
-#' @param status
-#' @param weights
-#' @param inits
-#' @param method
-#' @param control
-#' @param proportional
-#' @param params_greater_one
-#' @param ... Additional arguments to be passed to the function.
+#' @param .fit
+#' @param .timestart
+#' @param .timestop
+#' @param .status
+#' @param .weights
+#' @param .inits
+#' @param .method
+#' @param .control
+#' @param .proportional
+#' @param .params_greater_one
 #' @export
 #'
 
-wb_get_info <-
-  function(fit,
-           timestart,
-           timestop,
-           status,
-           weights = NULL,
-           inits = log(c(6,
-                         100)),
-           method = "Nelder-Mead",
-           control = list(fnscale = -1,
-                          parscale = c(0.1, 0.1),
-                          maxit = 2000),
-           proportional = TRUE,
-           params_greater_one = FALSE) {
+wb_get_info <- function(.fit
+                      , .timestart
+                      , .timestop
+                      , .status
+                      , .weights = NULL
+                      , .inits = log(c(6, 100))
+                      , .method = "Nelder-Mead"
+                      , .control = list(fnscale = -1
+                                        , .parscale = c(0.1, 0.1)
+                                        , .maxit = 2000)
+                      , .proportional = TRUE
+                      , .params_greater_one = FALSE
+    ){
+
     # Intitialize output-list
     output <- list()
-    
+
     # Retrieve the model coefficients
-    output[["model_coeff"]] <- rbind(fit[["coefficients"]])
-    
+    output[["model_coeff"]] <- rbind(.fit[["coefficients"]])
+
     # Add strata of the original data
-    output[["strata"]] <- pmisc::get_strata(fit)
-    
+    output[["strata"]] <- pmisc::get_strata(.fit)
+
     # Add strata_variable
     strata_vector <- levels(output[["strata"]])[1]
     output[["strata_variable"]] <-
       unlist(stringr::str_split(strata_vector,
                                 pattern = "="))[1]
     rm(strata_vector)
-    
+
     # Add linear predictor (just to ensure the manual calculation is right)
-    output[["lp"]] <- stats::predict(fit, type = "lp")
-    
+    output[["lp"]] <- stats::predict(.fit, type = "lp")
+
     # Add manually calculated lp_manual
-    output[["lp_manual"]] <- vector(length = nrow(fit[["model"]]))
-    names(output[["lp_manual"]]) <- rownames(fit[["model"]])
-    
+    output[["lp_manual"]] <- vector(length = nrow(.fit[["model"]]))
+    names(output[["lp_manual"]]) <- rownames(.fit[["model"]])
+
     ## Calculate means per stratum
     n_levels_strata <- nlevels(output[["strata"]])
     output[["means"]] <- list()
-    
+
     for (i in 1:n_levels_strata) {
       level <- levels(output[["strata"]])[i]
       v <- which(output[["strata"]] == level)
-      data_strata <- fit$model[v,]
+      data_strata <- .fit$model[v, ]
       output[["means"]][[i]] <-
         lapply(data_strata[, colnames(output[["model_coeff"]])], mean)
       names(output[["means"]])[i] <- level
       rm(v, level, data_strata)
     }
-    
+
     rm(i)
-    
-    lp_vector <- vector(length = length(fit[["coefficients"]]))
-    
-    for (i in 1:nrow(fit[["model"]])) {
-      stratum <- fit[["model"]][i, paste0("strata(",
+
+    lp_vector <- vector(length = length(.fit[["coefficients"]]))
+
+    for (i in 1:nrow(.fit[["model"]])) {
+      stratum <- .fit[["model"]][i, paste0("strata(",
                                           output[["strata_variable"]][1],
                                           ")")]
-      
+
       for (j in seq_along(output[["model_coeff"]])) {
         coefficient <- output[["model_coeff"]][j]
         mean <- unlist(output[["means"]][[stratum]][j])
         value <-
-          fit[["model"]][i, colnames(output[["model_coeff"]])[j]]
-        
+          .fit[["model"]][i, colnames(output[["model_coeff"]])[j]]
+
         lp_part <- (value - mean) * coefficient
-        
+
         lp_vector[j] <- lp_part
       }
-      
+
       output[["lp_manual"]][i] <- sum(lp_vector)
     }
-    
+
     # Add the Weibull parameters per stratum
     if (is.null(output[["strata"]])) {
-      output[["strata"]] <- factor(rep(1, nrow(fit[["model"]])))
+      output[["strata"]] <- factor(rep(1, nrow(.fit[["model"]])))
     }
-    
+
     n_levels <- nlevels(output[["strata"]])
-    
+
     wb_param <- stratum <- a <- b <- vector()
-    
+
     for (i in 1:n_levels) {
       v <- which(output[["strata"]] == levels(output[["strata"]])[i])
       wf <- pmisc::Wb_fit(
-        timestart = timestart[v],
-        timestop = timestop[v],
-        status = status[v],
+        timestart = .timestart[v],
+        timestop = .timestop[v],
+        status = .status[v],
         expLP = exp(output[["lp"]][v]),
-        weights = weights[v],
-        inits = inits,
-        method = method,
-        control = control,
-        proportional = proportional,
-        params_greater_one = params_greater_one
+        weights = .weights[v],
+        inits = .inits,
+        method = .method,
+        control = .control,
+        proportional = .proportional,
+        params_greater_one = .params_greater_one
       )
       a[v] <- wf$a
       b[v] <- wf$b
       stratum[v] <- levels(output[["strata"]])[i]
-      
+
       wb_param <- rbind(wb_param, wf)
       rownames(wb_param)[i] <- levels(output[["strata"]])[i]
     }
-    
+
     output[["wb_param_sample"]] <- cbind(stratum, a, b)
-    
+
     # Add the general Weibull parameters for simple use
     output[["wb_param"]] <- wb_param
-    
+
     # Add a complete parameter matrix per stratum
     output[["param_matrix"]] <- cbind(output[["wb_param"]],
                                       output[["model_coeff"]])
-    
+
     # Add the model to the output
-    output[["fit"]] <- fit
-    
+    output[["fit"]] <- .fit
+
     # Remove original data from model
     output[["fit"]][["model"]] <- NULL
-    
+
     # Return output
     output
   }
